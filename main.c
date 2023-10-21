220864b8fa997237db2f7385208b3cbcbf9489d2
Jocabin <kobyfar@hotmail.com>
2023-10-21 14:21:28 +0200

    trying to implement gitignore

--- main.c
+++ main.c
@@ -5,19 +5,72 @@
 #include <unistd.h>
 
 #define ArraySize(arr) sizeof(arr) / sizeof(arr[0])
-#define ArrayEnd(arr) arr[ArraySize(arr) - 1]
+#define true 1
+#define false 0
 
-char *FileExtensions[20] = {0};
+// TODO change this to dynamic
+char *FileExtensions[3] = {0};
 int TotalLinesCount = 0;
+const char *BasePath = NULL;
 
-void ReadDirectory(char *DirPath)
+int IsFileIgnored(char *FilePath, FILE *GIFile);
+void ReadDirectory(char *DirPath, char *GitIgnoreFilePath);
+
+int main(int argc, char **argv)
 {
+	char *DirPath = "./";
+
+	if (argv[1] != NULL)
+		DirPath = argv[1];
+
+	BasePath = realpath(DirPath, NULL);
+
+	if (argv[2] != NULL)
+	{
+		char *Ext;
+		int i = 0;
+
+		while ((Ext = strsep(&argv[2], ",")) != NULL)
+		{
+			if (FileExtensions[i] == NULL)
+				FileExtensions[i] = Ext;
+			++i;
+		}
+
+		fprintf(stdout, "Extensions allowed : ");
+		for (int idx = 0; idx < ArraySize(FileExtensions); ++idx)
+			if (FileExtensions[idx] != NULL)
+				fprintf(stdout, "[%s] ", FileExtensions[idx]);
+		fprintf(stdout, "\n\n");
+	}
+
+	ReadDirectory(DirPath, realpath(argv[3], NULL));
+
+	fprintf(stdout, "\nTotal Lines count : %d\n", TotalLinesCount);
+	return false;
+}
+
+void ReadDirectory(char *DirPath, char *GitIgnoreFilePath)
+{
+	FILE *GitIgnoreFile;
+
+	if (GitIgnoreFilePath != NULL)
+	{
+		GitIgnoreFile = fopen(GitIgnoreFilePath, "r");
+
+		if (GitIgnoreFile == NULL)
+		{
+			fprintf(stderr, "Can't read the .gitignore file => %s \n", GitIgnoreFilePath);
+			exit(1);
+		}
+	}
+
 	DIR *Directory;
 	struct dirent *DirEntity;
 
 	if (chdir(realpath(DirPath, NULL)))
 	{
-		fprintf(stderr, "Can't chande to %s directory\n", DirPath);
+		fprintf(stderr, "Can't move to %s directory\n", DirPath);
 		exit(1);
 	}
 
@@ -31,9 +84,14 @@ void ReadDirectory(char *DirPath)
 
 	while ((DirEntity = readdir(Directory)) != NULL)
 	{
+		if (strcmp(DirEntity->d_name, ".") == 0 || strcmp(DirEntity->d_name, "..") == 0)
+			continue;
+
 		char *CurrentFilePath = realpath(DirEntity->d_name, NULL);
 
-		if (DirEntity->d_type == DT_REG)
+		switch (DirEntity->d_type)
+		{
+		case DT_REG:
 		{
 			const char *dot = strrchr(CurrentFilePath, '.');
 
@@ -70,49 +128,53 @@ void ReadDirectory(char *DirPath)
 					fclose(CurrentFile);
 				}
 			}
+			break;
 		}
-		else if (DirEntity->d_type == DT_DIR)
+		case DT_DIR:
 		{
-			if (strcmp(DirEntity->d_name, ".") == 0 || strcmp(DirEntity->d_name, "..") == 0)
-				continue;
-			// TODO prendre en compte gitignore
-			ReadDirectory(realpath(DirEntity->d_name, NULL));
+			if (GitIgnoreFilePath || IsFileIgnored(realpath(DirEntity->d_name, NULL), GitIgnoreFile) == false)
+				ReadDirectory(realpath(DirEntity->d_name, NULL), GitIgnoreFilePath);
+			break;
+		}
+		default:
+			break;
 		}
 	}
+
 	chdir("..");
 	closedir(Directory);
+
+	if (GitIgnoreFile != NULL)
+		fclose(GitIgnoreFile);
 }
 
-int main(int argc, char *argv[])
+int IsFileIgnored(char *FilePath, FILE *GIFile)
 {
-	char *DirPath = "./";
+	char *CurrentGILine = NULL;
+	size_t LineLength = 0;
+	ssize_t Read;
 
-	if (argv[1] != NULL)
-		DirPath = argv[1];
-
-	if (argv[2] != NULL)
+	while ((Read = getline(&CurrentGILine, &LineLength, GIFile)) != -1)
 	{
-		char *Ext;
-		int i = 0;
-
-		while ((Ext = strsep(&argv[2], ",")) != NULL)
+		if (CurrentGILine != NULL)
 		{
-			if (FileExtensions[i] == NULL)
-				FileExtensions[i] = Ext;
-			++i;
-		}
+			char *PathToIgnore = strdup(BasePath);
 
-		fprintf(stdout, "Extensions allowed : ");
-		for (int idx = 0; idx < ArraySize(FileExtensions); ++idx)
-		{
-			if (FileExtensions[idx] != NULL)
-				fprintf(stdout, "[%s] ", FileExtensions[idx]);
+			// Remove LF char
+			CurrentGILine[Read - 1] = '\0';
+
+			// Add '/' to concat for a valid path
+			if (CurrentGILine[0] != '/')
+				PathToIgnore = strcat(PathToIgnore, "/");
+			PathToIgnore = strcat(PathToIgnore, CurrentGILine);
+
+			if (strcmp(PathToIgnore, FilePath) == 0)
+				return true;
 		}
-		fprintf(stdout, "\n\n");
 	}
 
-	ReadDirectory(DirPath);
+	if (CurrentGILine != NULL)
+		free(CurrentGILine);
 
-	fprintf(stdout, "\nTotal Lines count : %d\n", TotalLinesCount);
-	return 0;
+	return false;
 }
